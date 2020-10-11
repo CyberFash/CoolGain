865ed2292e5f2a8ae005e5130f83398b7a8be863
neche <chi2064real@gmail.com>
2020-10-11 15:08:58 +0100

    Initial commit

---
+++ app/src/main/java/nigeriandailies/com/ng/coolgain/MainActivity.java
@@ -0,0 +1,14 @@
+package nigeriandailies.com.ng.coolgain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
