b7823aca39dbd82f1d33d3f18b3fba547be7a28d
Okoro Godwin Chinedu <chinedu.go@gmail.com>
2020-10-17 18:39:54 +0100

    changed the background of the login screen! i have created the login page, the registeration page, the backend with firebase and also the landing page!

--- app/src/main/java/nigeriandailies/com/ng/coolgain/MainActivity.java
+++ app/src/main/java/nigeriandailies/com/ng/coolgain/MainActivity.java
@@ -2,14 +2,24 @@ package nigeriandailies.com.ng.coolgain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_main);
         getSupportActionBar ().hide ();
+    }
+
+    public void logout (View view){
+        FirebaseAuth.getInstance ().signOut (); //logging user out
+        startActivity (new Intent (getApplicationContext (), Login.class));
+        finish ();
     }
 }
